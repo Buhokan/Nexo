3a7535ee6eb44baddc5e8faea55f70b7005c8473
Buhokan <portadordiez1@gmail.com>
2026-07-14 01:26:16 -0500

    Add custom logo to Clerk login screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,9 @@ export default function RootLayout({
     <ClerkProvider
       localization={esES}
       appearance={{
+        layout: {
+          logoImageUrl: "/icons/icon-512.png",
+        },
         variables: {
           colorPrimary: "#7C5CFF",
           colorBackground: "#0B0B0F",
